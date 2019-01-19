528dd536d4d84ecdc2c5e3db01aab1e9077c05a6
Pierre_T <pierre.tsiakkaros@gmail.com>
2019-01-19 16:56:02 +0100

    #2 :sparkles: api/users/login post req
    #2 :sparkles:

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,11 +1,26 @@
 const express = require("express");
-const router = express.Router();
 const bcrypt = require("bcryptjs");
 const gravatar = require("gravatar");
+const jwt = require("jsonwebtoken");
 
+const router = express.Router();
+
+
+//* config
+const keys = require("./../../../config/keys");
+
+//*models
 const User = require("../../../models/User");
 
+//* Validation
 const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+
+
+
+
+
+//**ROUTES
 
 
 // ? @route GET to api/users/test
@@ -56,6 +71,51 @@ router.post("/register", (req, res) => {
         });
       });
     }
+  });
+});
+
+
+// ? @route GET to api/users/login
+// ? @description login user : returning JWT token
+// ! @ access Public
+
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      errors.email = "User not found";
+      return res.status(404).json(errors);
+    }
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        //User match
+        const payload = { id: user.id, name: user.name, avatar: user.avatar };
+
+        //Sign token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: `Bearer ${token}`,
+            });
+          },
+        );
+      } else {
+        errors.password = "Incorrect Password";
+        return res.status(400).json(errors);
+      }
+    });
   });
 });
 
